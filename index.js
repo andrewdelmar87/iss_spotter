c4ca1f452f673e405892a9da26b150e47a09dbde
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-05 03:07:36 +0000

    Added functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,10 +10,21 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP: ', ip);
 });
 
-// fetchCoordsByIP ('68.150.196.52', (error, coords) => {
-//   if (error) {
-//     console.log('It didn\'t work. ', error);
-//     return
-//   }
-//   console.log('It worked! Returned coords: ', coords);
-// });
+const coords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+  if (error) {
+    console.log('Error is: ', error);
+    return;
+  }
+
+  console.log('It worked! Flyover times: ', passTimes);
+})
+
+fetchCoordsByIP ('68.150.196.52', (error, coords) => {
+  if (error) {
+    console.log('It didn\'t work. ', error);
+    return
+  }
+  console.log('It worked! Returned coords: ', coords);
+});
